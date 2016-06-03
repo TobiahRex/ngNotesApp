f57b6058f66c7068799cb29dc005bfa6b7bc96c8
RexCorpDev <rexcorpfx@gmail.com>
2016-06-03 15:03:16 -0700

    backend notes complete - postman test tbd

--- server/models/note.js
+++ server/models/note.js
@@ -11,27 +11,31 @@ let noteSchema = new mongoose.Schema({
   title       :   {type : String}
 });
 
-noteSchema.statics.newNote = (noteobj, cb) => {
-  if(!noteInfo) return cb({ERROR : 'New note info INVALID.'});
+noteSchema.statics.newNote = (noteObj, cb) => { // noteObj = Full Schema Obj
+  if(!noteObj) return cb({ERROR : 'New note object INVALID.'});
   Note.create(noteObj, cb);
 };
 
-noteSchema.statics.locate = (noteId, cb) => {
+noteSchema.statics.locate = (noteId, cb) => { // noteId = '_id'
   if(!noteId) return cb({ERROR : 'Required ID not present.'});
   Note.findById(noteId, (err, dbNote) => {
     err ? cb(err) : cb(null, dbNote);
   });
 };
 
-noteSchema.statics.update = (noteInfo, cb) => {
-  if(!noteInfo) return cb({ERROR : 'NOTHING to append.'});
-  Note.findById(noteInfo.id, (err, dbNote)=>{
-    err ? cb(err) :
+noteSchema.statics.update = (updatedNote, cb) => { // updateNote = 'Full schema Obj to replace old Obj'.
+  if(!updatedNote) return cb({ERROR : 'NOTHING to append.'});
+  Note.findByIdAndUpdate(updatedNote._id, {$set : updatedNote }, (err, updatedDbNote)=>{
+    err ? cb(err) : cb(null, updatedDbNote);
   });
 };
 
-
-
+noteSchema.statics.remove = (noteId, cb) => {  // noteId = '_id'
+  if(!noteId) return cb({ERROR : 'Required ID not present.'});
+  Note.findByIdAndRemove(noteId, (err, oldNote) => {
+    err ? cb(err) : cb(null, {SUCCESS : 'Deleted!', NOTE : oldNote});
+  });
+};
 
 var Note = mongoose.model('Note', noteSchema);
 module.exports = Note;

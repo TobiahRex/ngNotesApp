ef1a235dbf186cefb9f575fc33cd13318011a077
RexCorpDev <rexcorpfx@gmail.com>
2016-06-03 18:18:29 -0700

    categories implemented inside note schema complete

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,6 @@ let express = require('express');
 let router  = express.Router();
 
 router.use('/users', require('./users'));
-router.use('/categories', require('./categories'));
 router.use('/notes', require('./notes'));
 
 module.exports = router;
--- server/routes/notes.js
+++ server/routes/notes.js
@@ -2,10 +2,31 @@
 
 let express = require('express');
 let router = express.Router();
+let Note = require('../models/note.js');
 
 router.route('/')
 .get((req, res) => {
-  res.send('all the notes');
+  Note.find({}, res.handle);
+})
+.delete((req, res) => {
+  Note.remove(res.handle);
+});
+
+
+router.route('/new')
+.post((req, res) => {
+  Note.newNote(req.body, res.handle);
+})
+
+router.route('/:id')
+.get((req, res) => {
+  Note.locate(req.params.id, res.handle);
+})
+.put((req, res) => {
+  Note.update({body : req.body, id : req.params.id}, res.handle);
+})
+.delete((req, res) => {
+  Note.removeOne(req.params.id, res.handle);
 });
 
 module.exports = router;

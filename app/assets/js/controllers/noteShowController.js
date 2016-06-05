253939649a61d13a373d718c74b548795f448429
RexCorpDev <rexcorpfx@gmail.com>
2016-06-04 20:33:29 -0700

    delete is working

--- app/assets/js/controllers/noteShowController.js
+++ app/assets/js/controllers/noteShowController.js
@@ -1,11 +1,17 @@
 'use strict';
 
 angular.module('NoteWrangler')
-.controller('NotesShowController', function(Note, $scope, $routeParams){
+.controller('NotesShowController', function(Note, $scope, $routeParams, $location){
 
   Note.get({id : $routeParams.id}, note => {
     $scope.note = note;
   }, response => { console.log(`ERROR:${response}`);});
+
+  $scope.deleteNote = note => {
+    note.$remove(() => {
+      $location.path('/notes');
+    });
+  };
 
   console.log('NotesShowController');
 });

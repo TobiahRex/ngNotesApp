e3322b198d70d70cb52da9473867c11dd987e7be
RexCorpDev <rexcorpfx@gmail.com>
2016-06-05 00:49:31 -0700

    added button statuses

--- app/assets/js/controllers/noteNewController.js
+++ app/assets/js/controllers/noteNewController.js
@@ -2,12 +2,16 @@
 
 angular.module('NoteWrangler')
 .controller('NotesNewController', function(Note, $scope, $location){
-
   $scope.note = new Note();
+  $scope.isSaving = false;
 
   $scope.newNote = note => {
+    $scope.isSaving = true;
     note.$save(() => {
+      $scope.isSaving = false;
       $location.path('/notes');
+    }, error => {
+      $scope.error = error;
     });
   };
 
